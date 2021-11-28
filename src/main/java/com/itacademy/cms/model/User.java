46c6aaa4bbd24c33be99a6832a0d9328f0eb0b1e
vkoni <konuhvladimir1@gmail.com>
2021-11-28 21:34:24 +0200

    User entity was created. Role enum was added. Hibernate validator dependency was added.

--- src/main/java/com/itacademy/cms/model/User.java
+++ src/main/java/com/itacademy/cms/model/User.java
@@ -1,28 +1,97 @@
 package com.itacademy.cms.model;
 
 import com.itacademy.cms.model.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @NotNull
     private String name;
 
+    @NotNull
     private String surname;
 
+    @NotNull
     private String email;
 
+    //Should be NotNull?
     private String password;
 
     private double accountCard;
 
+    @NotNull
     @Enumerated(EnumType.STRING)
     private Role role;
 
     private String about;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getAccountCard() {
+        return accountCard;
+    }
+
+    public void setAccountCard(double accountCard) {
+        this.accountCard = accountCard;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
 }
