ca458985637512add036948f3a47104f0f42046b
vkoni <konuhvladimir1@gmail.com>
2021-11-29 21:40:35 +0200

    User entity was updated with mapping. checkStyle file was configured. Base entity was created.

---
+++ src/main/java/com/itacademy/cms/model/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.itacademy.cms.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@MappedSuperclass
+@Getter
+@Setter
+public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
