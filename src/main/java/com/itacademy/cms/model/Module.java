3b1978f4fa25cf81f593831c798b9b1880f43a8b
izabor <izabor@softserveinc.com>
2021-11-29 18:58:18 +0200

    fixed annotations in Module(v0.2)

--- src/main/java/com/itacademy/cms/model/Module.java
+++ src/main/java/com/itacademy/cms/model/Module.java
@@ -1,59 +1,34 @@
 package com.itacademy.cms.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
 @Table(name = "modules")
 public class Module {
 
     @Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
-    @Getter
-    @Setter
     @Column(name = "id")
     private int id;
 
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "course_id")
-    @Getter
-    @Setter
     private Course course;
 
     @Column(name = "lesson_num")
-    @Getter
-    @Setter
-    private int lesson_num;
+    private int lessonNumber;
 
     @Column(name = "description")
-    @Getter
-    @Setter
     private String description;
 
     @Column(name = "content")
-    @Getter
-    @Setter
     private String content;
-
-    public Module() {
-    }
-
-    public Module(int lesson_num, String description, String content) {
-        this.lesson_num = lesson_num;
-        this.description = description;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Module{" +
-                "id=" + id +
-                ", lesson_num=" + lesson_num +
-                ", description='" + description + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
