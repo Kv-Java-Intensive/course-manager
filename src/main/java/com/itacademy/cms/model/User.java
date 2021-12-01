c24a4246f1020778221078d99536ecebd3fc5e7f
alex-tiutiunnyk <tytynnik6377@gmail.com>
2021-12-01 16:02:21 +0200

    Fix relations

--- src/main/java/com/itacademy/cms/model/User.java
+++ src/main/java/com/itacademy/cms/model/User.java
@@ -1,14 +1,8 @@
 package com.itacademy.cms.model;
 
 import com.itacademy.cms.model.enums.Role;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,7 +21,7 @@ public class User extends BaseEntity {
   @JoinColumn(name = "user_id")
   private List<UserToCourse> userCourse;
 
-  @ManyToMany(mappedBy = "users")
+  @ManyToMany(cascade = CascadeType.ALL)
   private List<Group> groups;
 
   @NotNull
