7f2f5b5f58fa4f95f061ae6f47e131f9b22f52f1
VKoniukh <konuhvladimir1@gmail.com>
2021-12-03 15:42:08 +0200

    User management functionality was added. Namely: post, put get all and get by id endpoints in controller. Conducive functional in UserServiceImpl also was implemented. +some cleaning

--- src/main/java/com/itacademy/cms/repository/GroupRepository.java
+++
@@ -1,7 +0,0 @@
-package com.itacademy.cms.repository;
-
-import com.itacademy.cms.model.Group;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupRepository extends JpaRepository<Group, Long> {
-}
