d1df80355ec7e61f4d428581a323355c45f9ca10
nikita_kosenko <nikitonkos2002@gmail.com>
2021-12-05 16:03:08 +0200

    Added user authentication from DB and enabled method security

--- src/main/java/com/itacademy/cms/configs/SecurityConfig.java
+++ src/main/java/com/itacademy/cms/configs/SecurityConfig.java
@@ -1,17 +1,31 @@
 package com.itacademy.cms.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  private final UserDetailsService userDetailsService;
+
+  @Autowired
+  public SecurityConfig(
+      @Qualifier("userDetailsServiceImpl") UserDetailsService userDetailsService) {
+    this.userDetailsService = userDetailsService;
+  }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
@@ -23,22 +37,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .clearAuthentication(true).permitAll();
   }
 
+
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth
-        .inMemoryAuthentication()
-        .withUser("user")
-        .password(getEncoder().encode("user"))
-        .roles("USER")
-        .and()
-        .withUser("admin")
-        .password(getEncoder().encode("admin"))
-        .roles("USER", "ADMIN");
+    auth.authenticationProvider(daoAuthenticationProvider());
   }
 
   @Bean
-  public PasswordEncoder getEncoder() {
+  protected PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder(12);
+  }
+
+  @Bean
+  protected DaoAuthenticationProvider daoAuthenticationProvider() {
+    DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+    daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+    daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+    return daoAuthenticationProvider;
   }
 
 }
--- src/main/java/com/itacademy/cms/repository/UserRepository.java
+++ src/main/java/com/itacademy/cms/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.itacademy.cms.repository;
 
 import com.itacademy.cms.model.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findByEmail(String email);
 }
