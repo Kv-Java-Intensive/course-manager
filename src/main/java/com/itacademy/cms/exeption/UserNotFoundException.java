444eed5daae18058d038b0605991e8865cb68cdb
VKoniukh <konuhvladimir1@gmail.com>
2021-12-05 20:49:53 +0200

    JUnit and Integration tests were created, inMemory DB was configured and user CRUD operations was checked.

--- src/main/java/com/itacademy/cms/exeption/UserNotFoundException.java
+++ src/main/java/com/itacademy/cms/exeption/UserNotFoundException.java
@@ -1,6 +1,6 @@
 package com.itacademy.cms.exeption;
 
-public class UserNotFoundException extends Exception {
+public class UserNotFoundException extends RuntimeException {
   public UserNotFoundException(String errorMessage) {
     super(errorMessage);
   }
