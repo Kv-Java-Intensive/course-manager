69c26a03df746cebaa6e63b8cbf82d3762d4231e
alex-tiutiunnyk <tiutiunnyk6377@gmail.com>
2021-12-06 17:17:51 +0200

    Delete unnecessary code

--- src/test/java/com/itacademy/cms/CmsApplicationTests.java
+++ src/test/java/com/itacademy/cms/CmsApplicationTests.java
@@ -3,7 +3,7 @@ package com.itacademy.cms;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = CmsApplicationTests.class)
+@SpringBootTest
 class CmsApplicationTests {
 
   @Test
