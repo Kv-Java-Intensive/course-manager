86eebc7beeae82e6cfe400b768b58362e30fcb07
olopus <olopus@softserveinc.com>
2021-12-06 21:40:20 +0200

    refresh course creation flow

---
+++ src/main/java/com/itacademy/cms/model/dto/CategoryDto.java
@@ -0,0 +1,12 @@
+//package com.itacademy.cms.model.dto;
+//
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+//@JsonIgnoreProperties
+//public class CategoryDto {
+//  private String category;
+//}
