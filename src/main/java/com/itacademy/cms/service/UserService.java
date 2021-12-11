fa8a8d8bad676f2e168d2f19d3d7bb39d29741b0
IvanZabor <90914301+IvanZabor@users.noreply.github.com>
2021-12-11 15:51:22 +0200

    Update UserService.java

--- src/main/java/com/itacademy/cms/service/UserService.java
+++ src/main/java/com/itacademy/cms/service/UserService.java
@@ -7,14 +7,10 @@ import java.util.List;
 public interface UserService {
 
   List<User> findAll();
-
-//  User findById(Long id);
-
+  
   void updateUser(UserDto userDto, String uuid);
 
   User saveUser(UserDto userDto);
-
-//  void deleteUserById(Long id);
 
   User findByUuid(String uuid);
 
