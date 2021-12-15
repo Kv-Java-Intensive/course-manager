903b51b0928932b1d2c4fc7d790fffcfefffaf99
alex-tiutiunnyk <tiutiunnyk6377@gmail.com>
2021-12-15 18:18:54 +0200

    add import

--- src/main/java/com/itacademy/cms/controller/CourseController.java
+++ src/main/java/com/itacademy/cms/controller/CourseController.java
@@ -2,6 +2,7 @@ package com.itacademy.cms.controller;
 
 import com.itacademy.cms.exeption.CourseNotFoundException;
 import com.itacademy.cms.mapper.MapStructMapper;
+import com.itacademy.cms.model.User;
 import com.itacademy.cms.model.dto.CourseGetDto;
 import com.itacademy.cms.model.dto.CoursePostDto;
 import com.itacademy.cms.model.dto.SearchCriteriaDto;
@@ -9,6 +10,7 @@ import com.itacademy.cms.service.CourseService;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
